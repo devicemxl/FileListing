e77a15296e4e76d519bf2cd98e0e27aa68bcb779
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2022-08-01 00:11:32 -0500

    Streamlit update
    filters tag, year, month, and more

--- FileListing.py
+++ FileListing.py
@@ -1,45 +1,84 @@
-from fnmatch import filter
 from os import listdir
 import pandas as pd
-from bs4 import BeautifulSoup
-#Paso 0. Definir la función para buscar archivos
-def BuscaArchivos(a,b):
+#
+import streamlit as st
+#
+#Paso 0. Inicia Streamlit
+#
+PAGE_TITLE = "File listing"
+st.set_page_config(
+    page_title = "File listing",
+    page_icon="🧊",
+    #layout="wide",
+    initial_sidebar_state="expanded",
+)
+#
+#Paso 1. Variables Iniciales
+#
+# función para buscar archivos
+def BuscaArchivos(a):
     e=[]
-    files= (filter(listdir(a), b))
+    files= listdir(a)
     for c in range(len(files)):
         d = files[c].split('.')
         e.append(d[0])
     return e
 # 
-filtro='*'
-#Paso 1. Generar lista de files en este directorio(folder)
-dirOriginal='C:/working/xampp/htdocs/Lab/05202002/daniel/original/'#pasar al inicio del script
+yyyy = [2019,2020,2021]
+mmmm = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+#
+#Paso 2 agregamos Filtros a utilizar
+#
+#st.sidebar.checkbox('yes')#st.sidebar.button('Click')#st.sidebar.radio('Pick your gender',['Male','Female'])
+year = st.sidebar.selectbox( 'Choose the year', yyyy )
+month = st.sidebar.selectbox( 'Choose the month', mmmm )
+#st.sidebar.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])#st.sidebar.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])#st.sidebar.slider('Pick a number', 0,50)
+#
+#Paso 3  se define los numeros que corresponden a cada mes, es decir la mascara en el path que corresponde a cada mes
+#
+#MonthMask = ['01_Jan', '02_Feb', '03_Mar', '04_Apr', '05_May', '06_Jun', '07_Jul', '08_Aug', '09_Sep', '10_Oct', '11_Nov', '12_Dec']
+MonthMask = ['02', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+for m in range(len(MonthMask)):
+    if month == mmmm[m]:
+        MonthValue = MonthMask[m] #Es el enmascarado del path que corresponde al mes del filtro
+#
+#Paso 4 Generar lista de files en este directorio(folder)
+dirOriginal='C:/working/xampp/htdocs/Lab/05202002/daniel/20220727/WorkPerformanceLoad/1_MONTHLY/1_Original/'+str(year)+'/'+str(year)+'-'+MonthValue+'/'#pasar al inicio del script
+#
+st.title ( PAGE_TITLE )
+st.header( str(year) + ' / ' + str(month) )
+#st.markdown("this is the header")#st.subheader("this is the subheader")#st.caption("this is the caption")#st.code("x=2021")#st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+#
+#Paso 5. Definir los archivos pendientes
+#
 files={} # Archivos pendientes identificados
-originalfiles = BuscaArchivos(dirOriginal, filtro)
+originalfiles = BuscaArchivos(dirOriginal)
 for each in originalfiles:
     files[str(each)]=''
 print('-----')
 print (files)
 #
-#Paso 2. Generar lista de files en este directorio (folder)
+#Paso 6. Generar lista de files en este directorio (folder)
 #
-dirCompleto='C:/working/xampp/htdocs/Lab/05202002/daniel/completo/'#pasar al inicio del script
-completedfiles=BuscaArchivos(dirCompleto,filtro)
+dirCompleto='C:/working/xampp/htdocs/Lab/05202002/daniel/20220727/WorkPerformanceLoad/1_MONTHLY/3_Completed/'+str(year)+'/'+str(year)+'-'+MonthValue+'/'#pasar al inicio del script
+completedfiles=BuscaArchivos(dirCompleto)
 for completed in completedfiles:
     files[completed] = 'completed'
 print('-----')
 print(files)
-#Paso 3. Tags #pasar al inicio del script
-root = 'C:/working/xampp/htdocs/Lab/05202002/daniel/DolordeCabeza/'#pasar al inicio del script
+#
+#Paso 7. Tags 
+#
+root = 'C:/working/xampp/htdocs/Lab/05202002/daniel/20220727/WorkPerformanceLoad/1_MONTHLY/2_Assigned/'#pasar al inicio del script
 types = [
-    '_dañado',
-    '_pateo_la_bolita',
-    '_problema1',
-    '_problema2'
+    '_INCLUDED IN',
+    '_MISSING SERIAL',
+    '_NEED APPROVAL',
+    '_OTRO PROBLEMA'
     ]#pasar al inicio del script
 #
 for tag in types:
-    tagfiles = BuscaArchivos( root + tag,filtro)
+    tagfiles = BuscaArchivos( root + tag)
     for q in files:
         if ( q in tagfiles ):
             files[q] = tag
@@ -54,18 +93,25 @@ print(files)
 for z in files.copy():
     if ( files[z] == 'completed' ):
         del(files[z])
-print(files)
 #
-table = pd.DataFrame.from_dict(files, orient='index', columns=['tags'])
+#Paso 8. Filtrar resultados
+#
+# definimos filtros
+TagsFilter = st.sidebar.multiselect('choose a tag',types, default=types)
+#st.sidebar.write('You selected:', ' / '.join(TagsFilter))
+# los files encontrados y clasificados son contratados con el filtro
+FilteredFiles={}
+for i in files:
+    if files[i] in TagsFilter:
+        #print(files[i])#tag
+        #print([i])#file
+        FilteredFiles[i]=files[i]
+#
+#Paso 8. Files filtrados a pandas 
+#
+table = pd.DataFrame.from_dict(FilteredFiles, orient='index', columns=['tags'])
 print('-----')
-print(table)
-table = table.to_html()
-
 #
-title = "Reporte X"#pasar al inicio del script
-top = open('render.html','r')
-footer=open('footer.html','r')
-html = str(BeautifulSoup(top,'lxml')) + '<h1>'+title+'</h1>' + table + str(BeautifulSoup(footer,'lxml'))
-report = open('report.html','w')
-report.writelines(html) 
-report.close()
+#Paso 9. Pandas a streamlit 
+#
+st.dataframe(data=table, width=None, height=None)
